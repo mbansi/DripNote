1f6e66db88c23b14bbee5e050a56ac7b2c2e3c82
Bansi Mamtora <mbansi306@gmail.com>
2022-05-31 10:58:48 +0530

    splash UI

---
+++ SplashScreen/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashViewController.swift
+//  DripNote
+//
+//  Created by Bansi Mamtora on 30/05/22.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
